dbb36f073beed55f4793503ae08fb49072264e1f
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-04-30 16:28:01 -0500

    values added

---
+++ Jscourse/script.js
@@ -0,0 +1,13 @@
+//Curso basico de js \
+//Pririves values 
+//Numbers 
+let number1=40;
+let string1='Hola soy Nico';
+//Empty values
+null
+undefined
+//Object type values
+let array1=[{name:'Nicolas',lastName:'Diaz',age:15 }]
+//typeof to get what is the value type of whatever I'm coding
+//typeof Null= OBJECT typeof Array=Object
+//Variables 
