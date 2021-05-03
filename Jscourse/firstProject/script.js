601c512de3985523b781b57ab32c68b714b5f2be
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-05-02 21:52:32 -0500

    done, just need to add the last function

--- Jscourse/firstProject/script.js
+++ Jscourse/firstProject/script.js
@@ -1,5 +1,5 @@
-var user_option=document.getElementById('#user_option')
-var result=document.getElementById('#result_text')
+
+var result=document.getElementById('result_text')
 var play_button=document.querySelector('.Juega')
 //Steps to create the game 
 
@@ -11,3 +11,71 @@ var play_button=document.querySelector('.Juega')
  
 //How to manipulate the options from the DOM
 // var x = document.getElementById("mySelect").options.item(0).text;
+//How to add text to the h3 
+// document.getElementById("demo").innerHTML = x;
+function validate(a,b){
+    // result.innerHTML='Holi'
+    //a and b have to be strings a=cpu,b=user 
+    if(a===b){
+        result.innerHTML='Play again'
+    }
+    else{
+        switch(a){
+            case 'Piedra':
+                switch(b){
+                    case 'Papel':
+                    console.log('You win')
+                    break;
+                    case 'Tijera':
+                    console.log('The machine wins')
+                    break;
+                } 
+            }  
+            switch(a){
+            case 'Tijera':
+                switch(b){
+                    case 'Papel':
+                    console.log('The machine wins')
+                    break;
+                    case 'Piedra':
+                    console.log('You win')
+                    break;
+                }    
+            }
+            switch(a){
+            case 'Papel':
+                switch(b){
+                    case 'Piedra':
+                    console.log('The machine wins')
+                    break;
+                    case 'Tijera':
+                    console.log('You win')
+                    break;
+                } 
+            }   
+    }
+}
+
+function play(){
+    const options_cpu=['Piedra','Papel','Tijera']
+    let cpu_option=options_cpu[Math.floor(Math.random()*3)];
+    const Piedra= document.getElementById('Piedra')
+    // const Piedra= document.getElementById('Piedra').value
+    const Papel= document.getElementById('Papel')
+    const Tijera= document.getElementById('Tijera')
+    // if(Piedra.checked==true){
+    //     validate(Piedra.value)
+    // }
+    const user_options_array=[Piedra,Papel,Tijera]
+    // console.log(user_options_array.length)
+    for(let i=0;i<user_options_array.length;i++){
+        if(user_options_array[i].checked===true){
+            const option=user_options_array[i].value
+            validate(option,cpu_option)
+            // validate(cpu_option,option)
+        }
+    }
+}
+
+
+play_button.addEventListener('click',function (){play()})
