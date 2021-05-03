19966fa79cfd28efdf08bf82a9f5bad6323a9bd8
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-05-02 19:58:23 -0500

    JS first functions added

---
+++ Jscourse/firstProject/script.js
@@ -0,0 +1,13 @@
+var user_option=document.getElementById('#user_option')
+var result=document.getElementById('#result_text')
+var play_button=document.querySelector('.Juega')
+//Steps to create the game 
+
+//Storage the function that the user creates
+//Create the logic ans test it in the console
+// 1. Create an array with the option the machine has
+// 2. Create a funcitin that evaluates how's the winner 
+// 3. Create a function that adds the text to the var result
+ 
+//How to manipulate the options from the DOM
+// var x = document.getElementById("mySelect").options.item(0).text;
