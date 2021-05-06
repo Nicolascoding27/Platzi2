ad76a87d4db8668f1974ea696fd9c845e7299c77
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-05-05 22:11:53 -0500

    while added

--- Jscourse/FundamentalsJs/script.js
+++ Jscourse/FundamentalsJs/script.js
@@ -1,154 +1,194 @@
-//Clase 1 => Variables \
+// //Clase 1 => Variables \
 
-//declaracion variables 
-var nombre;
-//Inicializacion variable
-nombre='Nico lindo';
-//; es simplemento una convencion hay casos puntuales en los que tenemos que agregar  el punto y coma
-console.log(nombre+' '+'rico')
-// Js es un lenguaje debilmnete tipado, esto hace que no concemos los tipos de las variables por lo cual debemos tener cuidado
-//Transformaciones de strings 
-var apellido="Diaz";
-var appellidoUpperCase=apellido.toUpperCase()
-//Primer caracter o caracteres 
-var firstletter=apellido.charAt(0)
-console.log(firstletter)
-//Cantidad de letras 
-var numberOfLetters=apellido.length
-console.log(numberOfLetters)
-//Interpolacion de variables 
-var fullName=`${appellidoUpperCase}+${firstletter}`
-console.log(fullName)
+// //declaracion variables 
+// var nombre;
+// //Inicializacion variable
+// nombre='Nico lindo';
+// //; es simplemento una convencion hay casos puntuales en los que tenemos que agregar  el punto y coma
+// console.log(nombre+' '+'rico')
+// // Js es un lenguaje debilmnete tipado, esto hace que no concemos los tipos de las variables por lo cual debemos tener cuidado
+// //Transformaciones de strings 
+// var apellido="Diaz";
+// var appellidoUpperCase=apellido.toUpperCase()
+// //Primer caracter o caracteres 
+// var firstletter=apellido.charAt(0)
+// console.log(firstletter)
+// //Cantidad de letras 
+// var numberOfLetters=apellido.length
+// console.log(numberOfLetters)
+// //Interpolacion de variables 
+// var fullName=`${appellidoUpperCase}+${firstletter}`
+// console.log(fullName)
 
-// Access to a substring
-var str= nombre.charAt(1)+nombre.charAt(2)
-//second way
-var srt2=nombre.substr(1,2)
-//Number variables
-var edad=27
-edad+=1
-//Restar
-edad-=1
-//Aproximar 
-Math.round()
-//pasar a estring 
-var total=294.4444444
-var total_round_3=total.toFixed(3)//3 decimales
-var totalString=parseFloat(total)
+// // Access to a substring
+// var str= nombre.charAt(1)+nombre.charAt(2)
+// //second way
+// var srt2=nombre.substr(1,2)
+// //Number variables
+// var edad=27
+// edad+=1
+// //Restar
+// edad-=1
+// //Aproximar 
+// Math.round()
+// //pasar a estring 
+// var total=294.4444444
+// var total_round_3=total.toFixed(3)//3 decimales
+// var totalString=parseFloat(total)
 
-//Funciones 
-//Las funciones sirven para reutilizar codigo
-//Alcance de las funciones, la idea es no modificar las variables globales 
-/// De esta manera lo que estamos haciendo es no modificar las variables globales
-// var nombre ='Nicolas'
-// function returnNameUpperCase(nombre){
-//     nombre=nombre.toUpperCase()
-//     console.log(nombre)
+// //Funciones 
+// //Las funciones sirven para reutilizar codigo
+// //Alcance de las funciones, la idea es no modificar las variables globales 
+// /// De esta manera lo que estamos haciendo es no modificar las variables globales
+// // var nombre ='Nicolas'
+// // function returnNameUpperCase(nombre){
+// //     nombre=nombre.toUpperCase()
+// //     console.log(nombre)
+// // }
+// // returnNameUpperCase(nombre)
+// //Objetos 
+// var Dario ={
+//     nombre:'Dario',
+//     apellido:'Diaz',
+//     age:15
 // }
-// returnNameUpperCase(nombre)
-//Objetos 
-var Dario ={
-    nombre:'Dario',
-    apellido:'Diaz',
-    age:15
-}
-var Nicolas ={
-    nombre:'Nicolas',
-    apellido:'Diaz',
-    age:21
-}
+// var Nicolas ={
+//     nombre:'Nicolas',
+//     apellido:'Diaz',
+//     age:21
+// }
 
-//Just getting the attribute name --NEW WAY TO DO IT 
-function nameToUpperCase({nombre}){
-    console.log(nombre.toUpperCase())
-}
-nameToUpperCase(Dario)
-nameToUpperCase(Nicolas)
+// //Just getting the attribute name --NEW WAY TO DO IT 
+// function nameToUpperCase({nombre}){
+//     console.log(nombre.toUpperCase())
+// }
+// nameToUpperCase(Dario)
+// nameToUpperCase(Nicolas)
 
-//Another way to call the function 
-function nameToUpperCase(persona){ //The patameter is an object
-    //var (nombre)
-    var { nombre }=persona//Im calling the attribute of the object here by using {}
-    console.log(nombre.toUpperCase())
-}
+// //Another way to call the function 
+// function nameToUpperCase(persona){ //The patameter is an object
+//     //var (nombre)
+//     var { nombre }=persona//Im calling the attribute of the object here by using {}
+//     console.log(nombre.toUpperCase())
+// }
 
-//solo hay un parametro el cual corresponde al objeto
-function nameAndAge(persona){
-    var {nombre}=persona
-    var{age}=persona
-    console.log(`Hola mi no3mbre es ${nombre},y tengo ${age}`)
-}
-nameAndAge(Dario)
+// //solo hay un parametro el cual corresponde al objeto
+// function nameAndAge(persona){
+//     var {nombre}=persona
+//     var{age}=persona
+//     console.log(`Hola mi no3mbre es ${nombre},y tengo ${age}`)
+// }
+// nameAndAge(Dario)
 
-function cumple(persona){
-    persona.age+=1
-    var {age}=persona
-    console.log(`mi edad es ${age}`)
-}//El objeto se ve modificado
-cumple(Nicolas)
-//Js behaves different with objects, los objetos que pasamos por paremtro se pasan por referencia
-///Como llamar un nuevo objecto que tenga los mismos atributos del objeto anterior
-//---Debemos retornar un objeto 
-function retornandoObjeto(persona){
-    return {
-        ...persona,//Aca es donde duplico el objeto
-        age:persona.age+=1
+// function cumple(persona){
+//     persona.age+=1
+//     var {age}=persona
+//     console.log(`mi edad es ${age}`)
+// }//El objeto se ve modificado
+// cumple(Nicolas)
+// //Js behaves different with objects, los objetos que pasamos por paremtro se pasan por referencia
+// ///Como llamar un nuevo objecto que tenga los mismos atributos del objeto anterior
+// //---Debemos retornar un objeto 
+// function retornandoObjeto(persona){
+//     return {
+//         ...persona,//Aca es donde duplico el objeto
+//         age:persona.age+=1
+//     }
+// }
+// retornandoObjeto(Dario)
+// //Tenemos dos opciones la primera es modificar un objeto o duplicarlo y cambiarle un atributo 
+// //--Parametros como referencia o valor 
+// function cumpleanos(persona)
+// //---COMPARACIONES EN VARIABLES 
+// var x =4;
+// var y='4'
+// x==y//true --It compares the intrinsic value but it does not compare the data type 
+// x===y//false-- It compares the value and the data type 
+// //Is it better to use the === instead of ==
+// var otraPersona={
+//     ...Dario
+// }
+// var test='this is a test'
+
+// // ---Condicionales----
+
+// var persona1={
+//     nombre:'Sasha',
+//     ingeniero:true,
+//     edad:19 
+// }
+// var persona2={
+//     nombre:'Nico',
+//     ingeniero:false ,
+//     edad:13
+// }
+
+// function imprimirProfesiones(persona){
+//     console.log(`${persona.nombre} es:`)
+//     persona.ingeniero ? console.log('Ingeniero') : console.log('pussy')
+// }
+// function imprimirSiesMayordeEdad(persona){
+//     persona.edad>18 ? console.log(`${persona.nombre} es mayor de edad`) : console.log(`${persona.nombre} es menor de edad`)
+// }
+// const MAYORIA_DE_EDAD=18
+// //this function returns true or false
+// function imprimirSiesMayordeEdad2(persona){
+//     return persona.edad>=18
+// }
+// imprimirSiesMayordeEdad(persona1)
+// imprimirSiesMayordeEdad(persona2)
+// var test=imprimirSiesMayordeEdad2(persona1)
+// console.log(test)
+
+// ///---ARROW FUNCTION----///
+// //Asign to a variable a function
+// var persona1={
+//     nombre:'Sasha',
+//     ingeniero:true,
+//     edad:19, 
+//     peso:75
+// }
+// const ES_MAYOR_DE_EDAD=persona=>{ //cuando solo tengo una variable no uso parentesis
+//     persona.edad > 18? console.log('I can drink') : console.log('I cant drink')
+// }
+// ES_MAYOR_DE_EDAD(persona1)
+// //--DECONSTRUCTING THE PARAMETER    
+// console.log(`Al inicio del año Sasha pesa ${persona1.peso}`)
+
+// function ciclo_edad(persona){
+// for(let i=1;i<356;i++){
+//     let random=Math.random()//numero entre 0 y 1`
+//     if(random<0.25){
+//         AUMENTAR_DE_PESO(persona)
+//     }
+//     else if(random<0.5){
+//         DISMINUIR_DE_PESO(persona)
+//     }
+// }  //aumenta de peso si es menor a 0.25 y si es menor a 0.5 adelgaza
+//     console.log(persona)
+// }
+
+const AUMENTAR_DE_PESO=persona=> persona.peso+=0.2 //flecha sin llaves hace que 
+const DISMINUIR_DE_PESO=(persona)=>{
+    persona.peso-=0.2
+}//Tofixed()solo lo que esta despues de la coma
+// ciclo_edad(persona1)
+///--while-------
+let persona1={
+    nombre:"nico",
+    peso:75
+}
+const COME_MUCHO= ()=>Math.random()<0.3
+const REALIZA_DEPORTE=()=>Math.random()<0.4//40% de los dias 
+let META_PESO= persona1.peso-3
+let dias=0
+while(persona1.peso>META_PESO){
+    if(COME_MUCHO()){
+        AUMENTAR_DE_PESO(persona1)
     }
+    if(REALIZA_DEPORTE()){
+        DISMINUIR_DE_PESO(persona1)
+    }
+    dias +=1               
 }
-retornandoObjeto(Dario)
-//Tenemos dos opciones la primera es modificar un objeto o duplicarlo y cambiarle un atributo 
-//--Parametros como referencia o valor 
-function cumpleanos(persona)
-//---COMPARACIONES EN VARIABLES 
-var x =4;
-var y='4'
-x==y//true --It compares the intrinsic value but it does not compare the data type 
-x===y//false-- It compares the value and the data type 
-//Is it better to use the === instead of ==
-var otraPersona={
-    ...Dario
-}
-var test='this is a test'
-
-// ---Condicionales----
-
-var persona1={
-    nombre:'Sasha',
-    ingeniero:true,
-    edad:19 
-}
-var persona2={
-    nombre:'Nico',
-    ingeniero:false ,
-    edad:13
-}
-
-function imprimirProfesiones(persona){
-    console.log(`${persona.nombre} es:`)
-    persona.ingeniero ? console.log('Ingeniero') : console.log('pussy')
-}
-function imprimirSiesMayordeEdad(persona){
-    persona.edad>18 ? console.log(`${persona.nombre} es mayor de edad`) : console.log(`${persona.nombre} es menor de edad`)
-}
-const MAYORIA_DE_EDAD=18
-//this function returns true or false
-function imprimirSiesMayordeEdad2(persona){
-    return persona.edad>=18
-}
-imprimirSiesMayordeEdad(persona1)
-imprimirSiesMayordeEdad(persona2)
-var test=imprimirSiesMayordeEdad2(persona1)
-console.log(test)
-
-///---ARROW FUNCTION----///
-//Asign to a variable a function
-var persona1={
-    nombre:'Sasha',
-    ingeniero:true,
-    edad:19 
-}
-const ES_MAYOR_DE_EDAD=persona=>{ //cuando solo tengo una variable no uso parentesis
-    persona.edad > 18? console.log('I can drink') : console.log('I cant drink')
-}
-ES_MAYOR_DE_EDAD(persona1)
-//--DECONSTRUCTING THE PARAMETER
+console.log(`Pasaron ${dias} dias hasta que Nick adelgazo 3kg`)
