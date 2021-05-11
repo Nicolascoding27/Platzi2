59840b7be563fc942859e02ac33dab608450639c
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-05-11 12:41:44 -0500

    object product added, probably we need to create a component called product

--- Jscourse/angular/platzi-store/src/app/app.component.ts
+++ Jscourse/angular/platzi-store/src/app/app.component.ts
@@ -6,5 +6,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'platzi-store';
+  title = '';
+  amigos=['']
+  bienvenida='Hola esta es mi Todo list ,agrega o borra lo que tienes que hacer' //esta variable puede ser llamada usando sting interpolation
+//Creacion del metodo para usar con el boton
+  addItem(){
+    this.amigos.push(this.title);//con el this hago referencia al array
+  }
+//Ahora necesitamos borrar asi que creamos un metodo de borrado
+//usamos typescript para tipear las variables ej: index:number
+deleteItem(index:number){
+  this.amigos.splice(index,1) //cuantos elementos deseo borrar, 1 posicion
+}
+//Vamos a recorrer un objeto que almacena las imagenes 
+products = [
+  {
+    id: '1',
+    image: 'assets/images/camiseta.png',
+    title: 'Camiseta',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '2',
+    image: 'assets/images/hoodie.png',
+    title: 'Hoodie',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '3',
+    image: 'assets/images/mug.png',
+    title: 'Mug',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '4',
+    image: 'assets/images/pin.png',
+    title: 'Pin',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '5',
+    image: 'assets/images/stickers1.png',
+    title: 'Stickers',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  },
+  {
+    id: '6',
+    image: 'assets/images/stickers2.png',
+    title: 'Stickers',
+    price: 80000,
+    description: 'bla bla bla bla bla'
+  }
+];
 }
