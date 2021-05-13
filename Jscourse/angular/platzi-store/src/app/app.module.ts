ffd8e7db51ee7d08023729462f7395f0702bd44f
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-05-13 12:24:15 -0500

    routes added

--- Jscourse/angular/platzi-store/src/app/app.module.ts
+++ Jscourse/angular/platzi-store/src/app/app.module.ts
@@ -4,14 +4,24 @@ import {FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import{ProductComponent} from './components/product.component';
-import { CartComponent } from './cart/cart.component' 
+import { CartComponent } from './cart/cart.component';
+import { PipedepipePipe } from './pipedepipe.pipe';
+import { HighlightDirective } from './highlight.directive';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { ContactComponent } from './contact/contact.component' 
 //Importo el componente para poder hacer uso de e'
 //Lo deobo poner en declarations para que angular lo reconozca 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    CartComponent
+    CartComponent,
+    PipedepipePipe,
+    HighlightDirective,
+    HomeComponent,
+    ProductsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
